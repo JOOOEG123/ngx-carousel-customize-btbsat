37d796400478840772cd77b51be408842ef9ae1c
JOOOEG123 <joeladeniji123@gmail.com>
2021-06-13 15:13:20 -0400

    1 file modified

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,25 +1,29 @@
+import { APP_BASE_HREF } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeRoutes } from "./Pages/Homepage";
 
-import { APP_BASE_HREF } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeRoutes } from './Pages/Homepage';
-
-const appRoutes: Routes = [
-  ...HomeRoutes
-]
-
+const appRoutes: Routes = [...HomeRoutes];
 
 @NgModule({
-  imports:      [ 
+  imports: [
     RouterModule.forRoot(appRoutes, {
-      anchorScrolling: 'enabled'
-    })
-    ],
-  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
-  exports: [ RouterModule ],
+      useHash: true,
+      onSameUrlNavigation: "reload",
+      scrollPositionRestoration: "enabled",
+    }),
+  ],
+  providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
+  exports: [RouterModule],
 })
-export class AppRoutesModule { 
-
-
+export class AppRoutesModule {
+  constructor() {
+    window.onload = () => {
+      window.scroll(0, +sessionStorage.getItem("scrollPosition") | 0);
+    };
+    window.onscroll = () => {
+      sessionStorage.setItem("scrollPosition", window.scrollY.toString());
+      console.log(window.screenY)
+    };
+  }
 }
-
